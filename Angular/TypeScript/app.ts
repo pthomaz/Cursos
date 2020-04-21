a6025512b1fc0f9729f4c2ddf81147342f470589
Pablo Thomaz <pablo.thomaz@b2egroup.com.br>
2020-04-21 11:08:42 -0300

    incluindo arquivos

---
+++ Angular/TypeScript/app.ts
@@ -0,0 +1,11 @@
+let message : string = "Help me"
+
+console.log(message);
+
+let episode : number = 4;
+console.log(episode);
+
+
+let favoriteDroid;
+favoriteDroid = "BB-8";
+console.log(favoriteDroid);
---
+++ Angular/TypeScript/dist/app.js
@@ -0,0 +1,7 @@
+var message = "Help me";
+console.log(message);
+var episode = 4;
+console.log(episode);
+var favoriteDroid;
+favoriteDroid = "BB-8";
+console.log(favoriteDroid);
