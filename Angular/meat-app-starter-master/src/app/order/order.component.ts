13841c62001cee8944aaab404aa4054b8405d129
Pablo Thomaz <pablo.thomaz@programmers.com.br>
2020-05-02 14:37:56 -0300

    ajuste

--- Angular/meat-app-starter-master/src/app/order/order.component.ts
+++ Angular/meat-app-starter-master/src/app/order/order.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  paymentOption : RadioOption[] = [
+    {label : 'Dinheiro', value: 'MON'},
+    {label : 'Cartão Crédito', value: 'CRED'},
+    {label : 'Cartão Débito', value: 'DEB'}
+  ];
 
   constructor() { }
 
